3ea04b5d42b04c26f474c7a9e9bd2a7afd3a4743
Aditya Kotkar <adityakotkar47@gmail.com>
2024-10-04 18:05:40 +0530

    bresenham line optimized

--- programs/bresenham_line_algorithm.cpp
+++ programs/bresenham_line_algorithm.cpp
@@ -6,10 +6,56 @@ void init() {
     gluOrtho2D(0, 500, 0, 500);
 }
 
-void putPixel(int x, int y) {
+void plotPoint(int x, int y) {
     glBegin(GL_POINTS);
         glVertex2i(x, y);
     glEnd();
+}
+
+void drawLine(int x1, int y1, int x2, int y2) {
+
+    if (x1 > x2) {
+        std::swap(x1, x2);
+        std::swap(y1, y2);
+    }
+
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+    int p;
+
+    int x = x1, y = y1;
+    plotPoint(x, y);
+    int yInc = (y2 > y1) ? 1 : -1;
+
+    if (dy < dx) {
+        p = 2*dy - dx;
+        
+        for (int i = 0; i < dx; i++) {
+            if (p < 0) {
+                p += 2*dy;
+            } else {
+                y += yInc;
+                p += 2*(dy - dx);
+            }
+            x += 1;
+            plotPoint(x, y);
+        }
+    }
+    else {
+        p = 2*dx - dy;
+
+        for (int i = 0; i < dy; i++) {
+            if (p < 0) {
+                p += 2*dx;
+            } else {
+                x += 1;
+                p += 2*(dx - dy);
+            }
+            y += yInc;
+            plotPoint(x, y);
+        }
+    }
+
 }
 
 void display() {
@@ -20,140 +66,7 @@ void display() {
     std::cout << "Enter [x1, y1, x2, y2]: ";
     std::cin >> x1 >> y1 >> x2 >> y2;
 
-    int dx = x2 - x1;
-    int dy = y2 - y1;
-
-    // Determine the absolute values for slope calculations
-    int abs_dx = abs(dx);
-    int abs_dy = abs(dy);
-
-    // Initialize decision parameter and starting point
-    int decision_param;
-    int x = x1, y = y1;
-
-    // Case I: Upward Slopes
-    if (dx >= 0 && dy >= 0) {
-        if (abs_dy <= abs_dx) {
-            decision_param = (2 * dy) - dx;
-
-            putPixel(x, y);
-            for (int _i = 0; _i < abs_dx; _i++) {
-                if (decision_param >= 0) {
-                    y += 1;
-                    decision_param += (2 * (dy - dx));
-                }
-                x += 1;
-                decision_param += (2 * dy);
-                putPixel(x, y);
-            }
-        } else { // |m| > 1
-            decision_param = (2 * dx) - dy;
-
-            putPixel(x, y);
-            for (int _i = 0; _i < abs_dy; _i++) {
-                if (decision_param >= 0) {
-                    x += 1;
-                    decision_param += (2 * (dx - dy));
-                }
-                y += 1;
-                decision_param += (2 * dx);
-                putPixel(x, y);
-            }
-        }
-    }
-
-    // Case II: Downward Slopes
-    else if (dx >= 0 && dy < 0) {
-        if (abs_dy <= abs_dx) {
-            decision_param = (2 * dy) + dx;
-
-            putPixel(x, y);
-            for (int _i = 0; _i < abs_dx; _i++) {
-                if (decision_param >= 0) {
-                    y -= 1;
-                    decision_param += (2 * (dy + dx));
-                }
-                x += 1;
-                decision_param += (2 * dy);
-                putPixel(x, y);
-            }
-        } else {
-            decision_param = (2 * abs_dx) + dy;
-
-            putPixel(x, y);
-            for (int _i = 0; _i < abs_dy; _i++) {
-                if (decision_param >= 0) {
-                    x += 1;
-                    decision_param += (2 * (abs_dx + dy));
-                }
-                y -= 1;
-                decision_param += (2 * abs_dx);
-                putPixel(x, y);
-            }
-        }
-    }
-
-    // Case III: Upward Slopes with negative slope
-    else if (dx < 0 && dy >= 0) {
-        if (abs_dy <= abs_dx) {
-            decision_param = (2 * dy) - abs_dx;
-
-            putPixel(x, y);
-            for (int _i = 0; _i < abs_dx; _i++) {
-                if (decision_param >= 0) {
-                    y += 1;
-                    decision_param += (2 * (dy - abs_dx));
-                }
-                x -= 1;
-                decision_param += (2 * dy);
-                putPixel(x, y);
-            }
-        } else { // |m| > 1
-            decision_param = (2 * abs_dx) - dy;
-
-            putPixel(x, y);
-            for (int _i = 0; _i < abs_dy; _i++) {
-                if (decision_param >= 0) {
-                    x -= 1;
-                    decision_param += (2 * (abs_dx - dy));
-                }
-                y += 1;
-                decision_param += (2 * abs_dx);
-                putPixel(x, y);
-            }
-        }
-    }
-
-    // Case IV: Downward Slopes with negative slope
-    else {
-        if (abs_dy <= abs_dx) {
-            decision_param = (2 * dy) + abs_dx;
-
-            putPixel(x, y);
-            for (int _i = 0; _i < abs_dx; _i++) {
-                if (decision_param >= 0) {
-                    y -= 1;
-                    decision_param += (2 * (dy + abs_dx));
-                }
-                x -= 1;
-                decision_param += (2 * dy);
-                putPixel(x, y);
-            }
-        } else { // |m| > 1
-            decision_param = (2 * abs_dx) + dy;
-
-            putPixel(x, y);
-            for (int _i = 0; _i < abs_dy; _i++) {
-                if (decision_param >= 0) {
-                    x -= 1;
-                    decision_param += (2 * (abs_dx + dy));
-                }
-                y -= 1;
-                decision_param += (2 * abs_dx);
-                putPixel(x, y);
-            }
-        }
-    }
+    drawLine(x1, y1, x2, y2);
 
     glFlush();
 }
